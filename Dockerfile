f433d716c1a9a3429e27857915221531dcd748a5
Gabriel P <mail.gabriel.pollak@gmail.com>
2023-03-11 10:17:49 +0200

    getting ready

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ ENV SSH_PRIVATE_FILE_PATH /var/jenkins_home/.ssh/keys/sq-proj1-ssh
 ENV AWS_ACCESS_KEY='<aws_access_key_id>'
 ENV AWS_SECRET_ACCESS_KEY='<aws_secret_access_key>'
 
-COPY --chown=jenkins:jenkins pipelines/*.groovy /var/jenkins_home/pipelines
+COPY --chown=jenkins:jenkins pipelines/*.groovy /var/jenkins_home/pipelines/
 COPY --chown=jenkins:jenkins plugins.txt /usr/share/jenkins/ref/plugins.txt
 COPY --chown=jenkins:jenkins sq-proj1-ssh /var/jenkins_home/.ssh/keys/sq-proj1-ssh
 
--- pipelines/aws-deploy.groovy
+++ pipelines/aws-deploy.groovy
@@ -1,91 +1,89 @@
 pipelineJob('AWS-Deploy') {
     definition {
-        sandbox {
-            cps {
-            script('''\
-                pipeline {
-                    agent { label 'Prod1' }
-                    environment {
-                        KEY = 'BOTH' // <<--- Set to 'Prod1', 'Prod2' or 'BOTH' to decide to which production server you'd like to deploy.
-                        
-                        AWS_CREDENTIALS = 'aws-Jenkins-Controller' // << aws credentials
-                        AWS_REGION = "eu-central-1" 
-                        AWS_BUCKET_NAME = "sq-proj1-bucket"
-                        
-                        GIT_SSH_CREDENTIALS = 'ssh-GitHub' // <<--- Git SSH Credentials.
-                        GIT_REPO_URL_HTTPS = "https://github.com/Inframous/project_1.git" 
-                        GIT_REPO_URL_SSH = "git@github.com:Inframous/project_1.git"
+        cps {
+        script('''\
+            pipeline {
+                agent { label 'Prod1' }
+                environment {
+                    KEY = 'BOTH' // <<--- Set to 'Prod1', 'Prod2' or 'BOTH' to decide to which production server you'd like to deploy.
+                    
+                    AWS_CREDENTIALS = 'aws-Jenkins-Controller' // << aws credentials
+                    AWS_REGION = "eu-central-1" 
+                    AWS_BUCKET_NAME = "sq-proj1-bucket"
+                    
+                    GIT_SSH_CREDENTIALS = 'ssh-GitHub' // <<--- Git SSH Credentials.
+                    GIT_REPO_URL_HTTPS = "https://github.com/Inframous/project_1.git" 
+                    GIT_REPO_URL_SSH = "git@github.com:Inframous/project_1.git"
+                }
+                stages {
+                    stage('Checkout SCM'){
+                        steps {
+                            checkout([
+                                $class: 'GitSCM',
+                                branches: [[name: 'main']],
+                                userRemoteConfigs : [[
+                                    url: "${GIT_REPO_URL_SSH}",
+                                    credentialsId: "${GIT_SSH_CREDENTIALS}"
+                                    ]]
+                                ])
+                        }
                     }
-                    stages {
-                        stage('Checkout SCM'){
-                            steps {
-                                checkout([
-                                    $class: 'GitSCM',
-                                    branches: [[name: 'main']],
-                                    userRemoteConfigs : [[
-                                        url: "${GIT_REPO_URL_SSH}",
-                                        credentialsId: "${GIT_SSH_CREDENTIALS}"
-                                        ]]
-                                    ])
-                            }
-                        }
-                        stage('Deploying App!') {
-                            steps {
-                                script {
-                                    try { 
-                                        sh """
-                                            sudo docker stop my-app-deployed
-                                            sudo docker rm -f my-app-deployed
-                                            sudo docker image rm my-app
-                                            ' 
-                                        """
-                                    } catch (err) { // Just incase it is the first run, catching the error or any others that might happen.
-                                        echo "There might have been an error stopping/removing the container/image..."
-                                        echo "If this is the first time running this job, you may ignore this."
-                                    } finally {
-                                        echo "Done with cleanup, deploying app..."
-                                    }
-                                    // Building and deploying the app in the Production Server.
-
-                                        // Not sure if I should clone the repo and if I should 'cd projec_1 or not'
+                    stage('Deploying App!') {
+                        steps {
+                            script {
+                                try { 
                                     sh """
-                                        sudo docker build -t my-app .
-                                        sudo docker run -d -p 80:80 --restart always --name my-app-deployed my-app
+                                        sudo docker stop my-app-deployed
+                                        sudo docker rm -f my-app-deployed
+                                        sudo docker image rm my-app
+                                        ' 
                                     """
+                                } catch (err) { // Just incase it is the first run, catching the error or any others that might happen.
+                                    echo "There might have been an error stopping/removing the container/image..."
+                                    echo "If this is the first time running this job, you may ignore this."
+                                } finally {
+                                    echo "Done with cleanup, deploying app..."
                                 }
-                            }
-                        }
-                        stage('Add item to DynamoDB') {
-                            // Adding the test results of the previous job to the DynamoDB created:
-                            // Downloading the csv from the bucket,
-                            // extracting and parsing the latest resulst, 
-                            // uploading the parsed data to an item within the DynamoDB table,
-                            // deleting the csv file.
-                            steps {
-                                withAWS(credentials: 'aws-Jenkins-Controller', region: 'eu-central-1') {
-                                    sh """
-                                        wget https://"${AWS_BUCKET_NAME}".s3.eu-central-1.amazonaws.com/report.csv
-                                        input=\$(tail -n 1 report.csv) 
-                                        TestId="\$((\$(wc report.csv -l | awk '{ print \$1 }') - 1))"
-                                        TestUser="\$(echo \$input | cut -d ',' -f1 | sed 's/ /-/g')"
-                                        TestDate="\$(echo \$input | cut -d ',' -f2 )"
-                                        TestResult="\$(echo \$input | cut -d ',' -f3 )"
+                                // Building and deploying the app in the Production Server.
 
-                                        echo \$TestID \$TestUser \$TestDate \$TestResults
-
-                                        aws dynamodb put-item \
-                                        --table-name TestTable \
-                                        --item \
-                                        '{"TestId": {"S": "'\$TestId'"}, "TestDate": {"S": "'\$TestDate'"}, "TestResult": {"S": "'\$TestResult'"}, "TestUser": {"S": "'\$TestUser'"}}' \
-                                        --return-consumed-capacity TOTAL
-                                        rm report.csv
-                                    """
-                                }
+                                    // Not sure if I should clone the repo and if I should 'cd projec_1 or not'
+                                sh """
+                                    sudo docker build -t my-app .
+                                    sudo docker run -d -p 80:80 --restart always --name my-app-deployed my-app
+                                """
                             }
                         }
                     }
-                }'''.stripIndent())
-            }
+                    stage('Add item to DynamoDB') {
+                        // Adding the test results of the previous job to the DynamoDB created:
+                        // Downloading the csv from the bucket,
+                        // extracting and parsing the latest resulst, 
+                        // uploading the parsed data to an item within the DynamoDB table,
+                        // deleting the csv file.
+                        steps {
+                            withAWS(credentials: 'aws-Jenkins-Controller', region: 'eu-central-1') {
+                                sh """
+                                    wget https://"${AWS_BUCKET_NAME}".s3.eu-central-1.amazonaws.com/report.csv
+                                    input=\$(tail -n 1 report.csv) 
+                                    TestId="\$((\$(wc report.csv -l | awk '{ print \$1 }') - 1))"
+                                    TestUser="\$(echo \$input | cut -d ',' -f1 | sed 's/ /-/g')"
+                                    TestDate="\$(echo \$input | cut -d ',' -f2 )"
+                                    TestResult="\$(echo \$input | cut -d ',' -f3 )"
+
+                                    echo \$TestID \$TestUser \$TestDate \$TestResults
+
+                                    aws dynamodb put-item \
+                                    --table-name TestTable \
+                                    --item \
+                                    '{"TestId": {"S": "'\$TestId'"}, "TestDate": {"S": "'\$TestDate'"}, "TestResult": {"S": "'\$TestResult'"}, "TestUser": {"S": "'\$TestUser'"}}' \
+                                    --return-consumed-capacity TOTAL
+                                    rm report.csv
+                                """
+                            }
+                        }
+                    }
+                }
+            }'''.stripIndent())
         }
     }
 }
