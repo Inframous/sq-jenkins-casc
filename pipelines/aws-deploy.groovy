b35614e65dba497064008a502f9777777f685eda
Gabriel P <mail.gabriel.pollak@gmail.com>
2023-03-11 21:03:48 +0200

    Done

--- pipelines/aws-deploy.groovy
+++ pipelines/aws-deploy.groovy
@@ -62,7 +62,7 @@ pipelineJob('AWS-Deploy') {
                         // deleting the csv file.
                         steps {
                             withAWS(credentials: 'aws-Jenkins-Controller', region: 'eu-central-1') {
-                                sh """
+                                sh '''
                                     wget https://"${AWS_BUCKET_NAME}".s3.eu-central-1.amazonaws.com/report.csv
                                     input=\$(tail -n 1 report.csv) 
                                     TestId="\$((\$(wc report.csv -l | awk '{ print \$1 }') - 1))"
@@ -78,7 +78,7 @@ pipelineJob('AWS-Deploy') {
                                     '{"TestId": {"S": "'\$TestId'"}, "TestDate": {"S": "'\$TestDate'"}, "TestResult": {"S": "'\$TestResult'"}, "TestUser": {"S": "'\$TestUser'"}}' \
                                     --return-consumed-capacity TOTAL
                                     rm report.csv
-                                """
+                                '''
                             }
                         }
                     }
